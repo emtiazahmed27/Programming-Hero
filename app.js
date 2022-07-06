83e782a1b59c0e0a44f3a728d6c4e4760d53e5cd
emtiazahmed27 <emtihstu127@gmail.com>
2022-07-06 23:00:02 +0600

    comitted

--- app.js
+++
@@ -1,7 +0,0 @@
-const button=document.querySelector(".mybtn"),
-paragraph=document.querySelector("p");
-
-paragraph.style.display="none";
-button.addEventListener("click",()=>{
-    paragraph.style.display="block";
-})
